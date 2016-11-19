8a10e3a7c625e5444dad48a19fbbc5ab569b75b0
DeSile <pl9lar@gmail.com>
2016-11-19 21:26:01 +0400

    First commit

---
+++ public/app.js
@@ -0,0 +1,28 @@
+var emaFront = angular.module("emaFront",[]);
+
+emaFront.controller("EmaCtrl",['$http', function($http){
+    var app = this;
+    console.log('init');
+
+
+    app.register = function(){
+        document.getElementById('content').src = 'http://localhost:9222';
+        console.log(document.getElementById('content'));
+    };
+
+    app.transferAuthorship = function(){
+        document.getElementById('content').src = 'http://localhost:9333/transferFileForm.html';
+        console.log(document.getElementById('content'));
+    };
+
+    app.addAudio = function(){
+        document.getElementById('content').src = 'http://localhost:9333/uploadFileForm.html';
+        console.log(document.getElementById('content'));
+    };
+
+    app.title = function(){
+        document.getElementById('content').src = 'main.html';
+        console.log(document.getElementById('content'));
+    }
+
+}]);
